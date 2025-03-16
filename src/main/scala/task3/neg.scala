8c284d40a9c8e2ec5043f196c917022f64cd9831
Mounir Samite <mounir.samite@gmail.com>
2025-03-16 18:31:38 +0100

    add generic neg function tdd

--- src/main/scala/task3/neg.scala
+++ src/main/scala/task3/neg.scala
@@ -2,13 +2,15 @@ package task3
 
 object neg extends App{
   val empty: String => Boolean = _ == ""
+  val zero: Int => Boolean = _ == 0
 
   val neg: (String => Boolean) => String => Boolean = pred => s => !pred(s)
 
+  def genericNeg[X](s: X, pred: (X => Boolean)) : Boolean =
+    !pred(s)
+
   val notEmpty = neg(empty)
 
-  println(notEmpty("ciao"))
-  println(neg(empty)(""))
 
 
 }
--- src/test/scala/task3/negTest.scala
+++ src/test/scala/task3/negTest.scala
@@ -7,8 +7,21 @@ import neg.*
 class negTest {
 
   val empty: String => Boolean = _ == ""
+  val zero: Int => Boolean = _ == 0
 
-  @Test def testnegFunc() =
+
+  @Test def testNegFunc(): Unit =
     assertFalse(neg(empty)(""))
     assertTrue(neg(empty)("ciao"))
+
+  @Test def testGenericNegFunc(): Unit =
+    val trueZero: Int = 0
+    val falseZero: Int = 9
+    val emptyString = ""
+    val notEmptyString = "ciao"
+    assertFalse(genericNeg(emptyString, empty))
+    assertTrue(genericNeg(notEmptyString, empty))
+    assertFalse(genericNeg(trueZero, zero))
+    assertTrue(genericNeg(falseZero, zero))
+
 }
